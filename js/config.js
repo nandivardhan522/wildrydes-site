1a53cfa6157cf6eb446178a49964b735339f2518
Bhumireddy Nandivardhan Reddy <nandivardhanreddybhumireddy@gmail.com>
2025-04-30 20:29:42 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lgPLZadMG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6r0p57tirt94vf07a2fiamnp6n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_G8nlL0Eec', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '61qufa73r0h84bdfhhe4ceb5qn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
